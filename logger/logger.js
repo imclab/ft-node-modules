af65021fd6a20d3c8571c32c90dee48c5d384404
richard.still <richard.still@ft.com>
2013-03-15 17:12:23 +0000

    Added some basic winston fucntionality and set some sensible defaults

--- logger/logger.js
+++ logger/logger.js
@@ -1,114 +1,45 @@
 var winston = require("winston"),
-    Konsole = require("konsole");
+    Konsole = require("konsole"),
+    ftUtils = require("../lib/utils.js");
 
-//winston.add(require('winston-loggly').Loggly, options);
-//winston.add(require('winston-splunk').splunk, options);
+// Configuration options for the logger module
+var config = {
+    logDir: 'logs/',
+    logFile: 'console.log',
+    winstonLoglevel: 'warn',
+    logglyKey: null,
+    logglyDomain: null,
+    getFileLogPath: function () {
+        return this.logDir + this.logFile;
+    }
+};
 
-//var logentries = require("node-logentries");
-//var log = logentries.logger({
-//
-//});
-//// use as a winston transport
-//log.winston( winston, {level:"info"} );
-
-// have to remove the console logger otherwise recursion occurs because we're overriding console.* using Konsole (and winston.transports.Console uses console.log)
+// We have to remove the console logger otherwise recursion occurs because we're overriding console.* using Konsole (and winston.transports.Console uses console.log)
 winston.remove(winston.transports.Console);
 
+// Add the file transport
 winston.add(winston.transports.File, {
-    filename: "logs/console.log",
+    filename: config.getFileLogPath(),
     //colorize: true,
     timestamp: true,
     maxsize: 52428800, //50Mb
     maxFiles: 1,
+    level: config.winstonLoglevel,
     json: true
 });
 
-// winston.add(winston.transports.Loggly, {
-//     subdomain: "financialtimes",
-//     inputToken: "98cf7d67-90bf-4a93-839c-cff4c33bf568",
-//     json: true
-// });
+// Add the loggly transport
+winston.add(require('winston-loggly').Loggly, {
+    subdomain: config.logglyDomain,
+    inputToken: config.logglyKey,
+    level: config.winstonLoglevel,
+    json: true
+});
 
-// winston.add(winston.transports.splunk, {
-//         splunkHostname: "node-server"
-//         //,splunkPort:""
-// });
-
-
-//winston.loggers.options.transports = [
-//    // Setup your shared transports here
-//    new winston.transports.Console({
-//        level: "info",
-//        colorize: true,
-//        timestamp: true
-//    }),
-//
-//    new winston.transports.File({
-//        filename: Env.path.LOGS_DIR + "console.log",
-//        //colorize: true,
-//        timestamp: true,
-//        maxsize: 52428800, //50Mb
-//        maxFiles: 1,
-//        json: true
-//    }),
-//
-//    new winston.transports.Loggly({
-//        subdomain: "financialtimes",
-//        inputToken: "98cf7d67-90bf-4a93-839c-cff4c33bf568",
-//        json: true
-//    })
-//
-//// http://blogs.splunk.com/2012/09/28/meet-your-splunk-hackathon-winners-splunk-for-winston
-//// https://github.com/erichelgeson/winston-splunk
-//
-////    new winston.transports.splunk({
-////        splunkHostname: "node-server"
-////        //,splunkPort:""
-////
-////    })
-//];
-
-//var logger = new (winston.Logger)({
-//    transports: [
-////        new winston.transports.Console({
-////            level: "info",
-////            colorize: true,
-////            timestamp: true
-////        }),
-//
-//        new winston.transports.File({
-//            filename: Env.path.LOGS_DIR + "console.log",
-//            //colorize: true,
-//            timestamp: true,
-//            maxsize: 52428800, //50Mb
-//            maxFiles: 1,
-//            json: true
-//        }),
-//
-//        new winston.transports.Loggly({
-//            subdomain: "financialtimes",
-//            inputToken: "98cf7d67-90bf-4a93-839c-cff4c33bf568",
-//            json: true
-//        }),
-//
-//// http://blogs.splunk.com/2012/09/28/meet-your-splunk-hackathon-winners-splunk-for-winston
-//// https://github.com/erichelgeson/winston-splunk
-//
-//        new winston.transports.splunk({
-//            splunkHostname: "node-server"
-//            //,splunkPort:""
-//
-//        })
-//
-//    ]
-////    ,
-////    exceptionHandlers: [
-////        new winston.transports.File({ filename: Env.path.LOGS_DIR + "exceptions.log" })
-////    ]
-//});
-//
-//exports.logger = logger;
-
+exports.init = function (passedConfig) {
+    config = ftUtils.mergeConfig(config, passedConfig);
+    console.log(config);
+};
 
 var restoreConsole = require("konsole/overrideConsole");
 ////
@@ -130,28 +61,51 @@ console.on('message', function (level, args) {
     // this.format      - shortcut to util.format
 
     var trace = this.trace; // trace is a getter, if you do not access the property it will not generate a trace
+    var msg = this.format.apply(this, args);
+    this.write(msg);
 
-    this.write(this.format.apply(this, args));
-
-    winston.log(level, this.format.apply(this, args), {
+    winston.log(level, {
+        msg: msg,
         pid: this.pid,
-        processType: this.procesType,
+        processType: this.processType,
         label: this.label,
         path: trace.path,
         line: trace.line
     });
 });
-//
+
+
 console.on('error', function (args) {
     // need to handle this otherwise Error: Uncaught, unspecified 'error' event.
 });
-//console.on('log', function ( args) {
+
+
+//winston.add(require('winston-splunk').splunk, options);
+
+//var logentries = require("node-logentries");
+//var log = logentries.logger({
 //
 //});
+//// use as a winston transport
+//log.winston( winston, {level:"info"} );
 
-var value = {obj: "two"};
-console.log("LOG SAMPLE");
-console.info("INFO SAMPLE  %s", value.obj);
-console.error("ERROR SAMPLE");
-//winston.info("HELLO")
+// winston.add(winston.transports.Loggly, {
+//     subdomain: "financialtimes",
+//     inputToken: "98cf7d67-90bf-4a93-839c-cff4c33bf568",
+//     json: true
+// });
 
+// winston.add(winston.transports.splunk, {
+//         splunkHostname: "node-server"
+//         //,splunkPort:""
+// });
+
+//// http://blogs.splunk.com/2012/09/28/meet-your-splunk-hackathon-winners-splunk-for-winston
+//// https://github.com/erichelgeson/winston-splunk
+//
+////    new winston.transports.splunk({
+////        splunkHostname: "node-server"
+////        //,splunkPort:""
+////
+////    })
+//];
